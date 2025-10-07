ebe13f14e6dc69794cabf7ab4f71a7480e529806
BanuKarakaya <nurbanukarakaya03@gmail.com>
2025-10-07 15:42:03 +0300

    Initial Commit

---
+++ WorldSideTests/WorldSideTests.swift
@@ -0,0 +1,17 @@
+//
+//  WorldSideTests.swift
+//  WorldSideTests
+//
+//  Created by Banu Karakaya on 7.10.2025.
+//
+
+import Testing
+@testable import WorldSide
+
+struct WorldSideTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
